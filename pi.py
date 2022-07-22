cef0c30f1a6687ff3cc28da59bdb9c81f245853e
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-07-22 00:35:21 -0400

    Fixed code?

--- pi.py
+++ pi.py
@@ -1,7 +1,8 @@
 digits = int(input("To what digit do you want pi rounded to up to a million"))
 digits += 2
 # I added two to make up for the decimal point and the 3
-f = open("/home/artemis/vscodefiles/python/pi/pi.txt", "r")
+f = open("pi.txt", "r")
 pi = f.readline()
+f = close("pi.txt")
 print(pi[0 : digits])
 print(len("3.14159265"))
